0ffb65359b801ededa359eb81e95b62afba9cbe1
Sushant <sushantberiwal@gmail.com>
2021-04-21 20:38:01 +0530

    created extensions folder

--- Module1/Lec2_FolderSorting/activity/folderSort.js
+++ Module1/Lec2_FolderSorting/activity/folderSort.js
@@ -1,3 +1,58 @@
+let fs = require("fs");
+
 let extensionsMapping = require("./util.js");
 
-console.log(extensionsMapping);
+
+let testFolderPath = "./Downloads";
+let allFiles = fs.readdirSync(testFolderPath);
+// console.log(allFiles);
+
+for(let i=0 ; i<allFiles.length ; i++){
+    sortFile(allFiles[i]);
+}
+
+
+function getExtension(file){
+    file = file.split(".");
+    return file[1];
+}
+
+// {
+//     "Documents" : ["doc" , "pdf" , "txt" ],
+//     "Images": ["img" , "jpg" , "gif" , "png"],
+//     "Audio": ["mp3"] ,
+//     "Video": ["mp4" , "mkv"],
+//     "Applications" : ["exe"]
+// }
+
+function checkExtensionFolder(extension){
+    // extension = "doc";
+    let extensionFolderName = testFolderPath;
+    for(let key in extensionsMapping){
+        let extensions = extensionsMapping[key];
+        if(extensions.includes(extension)){
+            extensionFolderName = extensionFolderName+"/"+key;
+            break;
+        }
+    }
+    // extensionFolderName = 'Documents'
+    // "./Downloads"
+    // let folderToBeChecked = testFolderPath+"/"+extensionFolderName;
+    // "./Downloads/Documents"
+    let isFolderExist =  fs.existsSync(extensionFolderName);
+    if(!isFolderExist){
+        fs.mkdirSync(extensionFolderName);
+    }
+}
+
+function moveFile(file , extension){
+
+}
+
+
+function sortFile(file){
+    let extension = getExtension(file);
+    // console.log(extension);
+    checkExtensionFolder(extension);
+    // moveFile();
+}
--- Module1/Lec2_FolderSorting/activity/util.js
+++ Module1/Lec2_FolderSorting/activity/util.js
@@ -6,3 +6,4 @@ module.exports = {
     "Applications" : ["exe"]
 }
 
+

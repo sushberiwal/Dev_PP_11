940cfdc4081243f2b1db45e61051aec57336d8da
Sushant <sushantberiwal@gmail.com>
2021-04-21 18:37:34 +0530

    fs demo

---
+++ Module1/Lec2_FolderSorting/facts/fs.js
@@ -0,0 +1,14 @@
+// fs => file System
+
+const fs = require("fs");
+
+// console.log(fs);
+
+// utf-8 => format for plain text !!
+
+let f1KaData = fs.readFileSync("./f1.txt");
+console.log(f1KaData + "");
+
+
+fs.writeFileSync("index.html" , "Hello world !!!");
+

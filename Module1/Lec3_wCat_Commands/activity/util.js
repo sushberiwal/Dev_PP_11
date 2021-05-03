7c597ae76d102b4e74ae97360d284a94d99e5f55
Sushant <sushantberiwal@gmail.com>
2021-05-03 21:17:05 +0530

    wCat commands complete

---
+++ Module1/Lec3_wCat_Commands/activity/util.js
@@ -0,0 +1,75 @@
+let fs = require("fs");
+function getFilesData(files) {
+  let filesData = "";
+  for (let i = 0; i < files.length; i++) {
+    if (!fs.existsSync(files[i])) {
+      console.log("One or more Files Doesn't Exist !");
+      return;
+    }
+    if (i == files.length - 1) {
+      filesData += fs.readFileSync(files[i]);
+    } else {
+      filesData += fs.readFileSync(files[i]) + "\r\n";
+    }
+  }
+  return filesData;
+}
+function applySFlag(data) {
+  // Hey I am F1
+  // space
+  // space
+  // space
+  // space
+  // space
+  // Bye I am F1
+  let emptyIncluded = false;
+  let removedSpaces = [];
+  let splittedData = data.split("\r\n");
+  //   [ 'Hey I am F1', '', '', '', '',  '','Bye I am F1', '' , "Hey m also f1"];
+  //   splittedData.length = 9
+  // i=6;
+
+  for (let i = 0; i < splittedData.length; i++) {
+    if (splittedData[i] == "" && emptyIncluded == false) {
+      removedSpaces.push(splittedData[i]);
+      emptyIncluded = true;
+    } else if (splittedData[i] != "") {
+      removedSpaces.push(splittedData[i]);
+      if (i < splittedData.length - 2) emptyIncluded = false;
+    }
+  }
+  let removedSpacesString = removedSpaces.join("\r\n");
+  return removedSpacesString;
+  // Hey I am F1
+  // space
+  // Bye I am F1
+}
+function applyBFlag(data) {
+  let count = 1;
+  let splittedData = data.split("\r\n");
+  for (let i = 0; i < splittedData.length; i++) {
+    if (splittedData[i] != "") {
+      splittedData[i] = `${count}. ${splittedData[i]}`;
+      // splittedData[i] = count+". "+splittedData[i];
+      count++;
+    }
+  }
+  // console.log(splittedData);
+  let bFlaggedString = splittedData.join("\n");
+  return bFlaggedString;
+}
+function applyNFlag(data) {
+  let count = 1;
+  let splittedData = data.split("\r\n");
+  for (let i = 0; i < splittedData.length; i++) {
+    splittedData[i] = `${count}. ${splittedData[i]}`;
+    count++;
+  }
+  // console.log(splittedData);
+  let nFlaggedString = splittedData.join("\n");
+  return nFlaggedString;
+}
+module.exports.getFilesData = getFilesData;
+module.exports.applySFlag = applySFlag;
+module.exports.applyBFlag = applyBFlag;
+module.exports.applyNFlag = applyNFlag;
--- Module1/Lec3_wCat_Commands/activity/wCat.js
+++ Module1/Lec3_wCat_Commands/activity/wCat.js
@@ -1,16 +1,48 @@
+const { getFilesData, applySFlag, applyBFlag, applyNFlag } = require("./util");
 let contents = process.argv.slice(2);
 // console.log(contents);
 
 const flags = [];
 const files = [];
 
-for(let i=0 ; i<contents.length ; i++){
-    if(contents[i].startsWith("-")){
-        flags.push(contents[i])
-    }else{
-        files.push(contents[i]);
-    }
+for (let i = 0; i < contents.length; i++) {
+  if (contents[i].startsWith("-")) {
+    flags.push(contents[i]);
+  } else {
+    files.push(contents[i]);
+  }
+}
+// -s -b -n
+// f1.txt // f2.txt
+
+let filesData = getFilesData(files);
+
+if (flags.includes("-s")) {
+  // filesData updated if s flag is present !
+  filesData = applySFlag(filesData);
 }
 
-console.log(flags);
-console.log(files);
+// console.log(filesData);
+
+// when both -b and -n flags are present
+if (flags.includes("-b") && flags.includes("-n")) {
+  if (flags.indexOf("-b") < flags.indexOf("-n")) {
+    // apply b flag
+    filesData = applyBFlag(filesData);
+  } else {
+    // apply n flag
+    filesData = applyNFlag(filesData);
+  }
+}
+// only -b flag is present
+else if (flags.includes("-b")) {
+  // apply b flag
+  filesData = applyBFlag(filesData);
+}
+// only -n flag is present
+else if (flags.includes("-n")) {
+  // apply n flag
+  filesData = applyNFlag(filesData);
+}
+
+console.log(filesData);

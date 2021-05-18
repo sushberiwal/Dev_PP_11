54efe11bf95075f39db6f702eb7e56d41a273529
Sushant <sushantberiwal@gmail.com>
2021-05-18 19:27:35 +0530

    promises for multiple files

---
+++ Module1/Lec7_Promises/nFiles/asyncParallel.js
@@ -0,0 +1,2 @@
+let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+
---
+++ Module1/Lec7_Promises/promisesSerial.js
@@ -0,0 +1,19 @@
+// Serially read contents of f1 f2 and f3 using promisified function
+
+const fs = require("fs");
+
+// promise hell
+
+let f1KaPromise = fs.promises.readFile("./f1.txt");
+
+f1KaPromise.then(function(data){
+    console.log(data+"");
+    let f2KaPromise = fs.promises.readFile("./f2.txt");
+    f2KaPromise.then(function(data){
+        console.log(data+"");
+        let f3KaPromise = fs.promises.readFile("./f3.txt");
+        f3KaPromise.then(function(data){
+            console.log(data+"");
+        })
+    })
+})

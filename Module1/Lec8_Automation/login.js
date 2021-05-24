e2065a52c10ac66015aabd02696ca881c3216f95
sushant <sushantberiwal@gmail.com>
2021-05-24 19:02:04 +0530

    logged in hackerrank

--- Module1/Lec8_Automation/login.js
+++ Module1/Lec8_Automation/login.js
@@ -1,21 +1,36 @@
 const puppeteer = require("puppeteer");
+const id = "misid46226@isecv.com";
+const pw = "123456789";
+let tab;
 
 // puppeteer has promisfied functions
 
 // by default headless = true
 
-let browserOpenPromise = puppeteer.launch({ headless: false });
-console.log(browserOpenPromise);
-// Promise<Pending>
-browserOpenPromise.then( function(browser){
+let browserOpenPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+});
+
+browserOpenPromise
+  .then(function (browser) {
     console.log("browser is opened !");
     return browser.pages();
-})
-.then(function(pages){
-    let tab = pages[0];
-    return tab.goto("https://www.google.com");
-})
-.then(function(){
-    console.log("On google homepage !!");
-})
-
+  })
+  .then(function (pages) {
+    tab = pages[0];
+    return tab.goto("https://www.hackerrank.com/auth/login");
+  })
+  .then(function(){
+      return tab.type("#input-1" , id);
+  })
+  .then(function(){
+      return tab.type("#input-2" , pw);
+  })
+  .then(function(){
+      return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+  })
+  .then(function(){
+      console.log("logged in !!!");
+  })

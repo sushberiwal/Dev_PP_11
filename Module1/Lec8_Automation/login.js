06c8153528d6f81a6ae3d9ea3b680fc3014e611c
sushant <sushantberiwal@gmail.com>
2021-05-24 19:40:25 +0530

    reached warmup page

--- Module1/Lec8_Automation/login.js
+++ Module1/Lec8_Automation/login.js
@@ -22,15 +22,28 @@ browserOpenPromise
     tab = pages[0];
     return tab.goto("https://www.hackerrank.com/auth/login");
   })
-  .then(function(){
-      return tab.type("#input-1" , id);
+  .then(function () {
+    return tab.type("#input-1", id);
   })
-  .then(function(){
-      return tab.type("#input-2" , pw);
+  .then(function () {
+    return tab.type("#input-2", pw);
   })
-  .then(function(){
-      return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+  .then(function () {
+    return tab.click(
+      ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"
+    ); // login hojata hai click se
   })
-  .then(function(){
-      console.log("logged in !!!");
+//   wait and click !!
+  .then(function () {
+    return tab.waitForSelector("#base-card-1-link", { visible: true });
   })
+  .then(function () {
+    return tab.click("#base-card-1-link");
+  })
+//   wait and click !!
+  .then(function () {
+    return tab.waitForSelector('a[data-attr1="warmup"]', { visible: true });
+  })
+  .then(function () {
+    return tab.click('a[data-attr1="warmup"]');
+  });

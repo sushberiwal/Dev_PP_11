c0bbc128f3df72e085bf276e4861d35cc639e4ba
sushant <sushantberiwal@gmail.com>
2021-05-26 19:55:12 +0530

    create challenge started

--- Module1/Lec9_Async_Await/activity/login.js
+++ Module1/Lec9_Async_Await/activity/login.js
@@ -1,8 +1,9 @@
 const puppeteer = require("puppeteer");
 const id = "misid46226@isecv.com";
 const pw = "123456789";
+let challenges = require("./challenges");
 
-(async function(){
+async function login(){
     let browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
@@ -14,5 +15,36 @@ const pw = "123456789";
     await tab.type("#input-1", id);
     await tab.type("#input-2", pw);
     await tab.click( ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
-    console.log("Logged in !!");
-})();
+    await tab.waitForSelector('div[data-analytics="NavBarProfileDropDown"]' , {visible:true});
+    await tab.waitForTimeout(2000);
+    let element = await tab.$('div[data-analytics="NavBarProfileDropDown"]');
+    await element.click();
+    await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
+    await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
+    await tab.waitForSelector('.nav-tabs.nav.admin-tabbed-nav li' , {visible:true});
+    let bothLis = await tab.$$('.nav-tabs.nav.admin-tabbed-nav li');
+    let manageChallengeLi = bothLis[1];
+    await manageChallengeLi.click();
+    await tab.waitForSelector('.btn.btn-green.backbone.pull-right' , {visible:true});
+    let createChallengeElement = await tab.$('.btn.btn-green.backbone.pull-right');
+    let createChallengeLink = await tab.evaluate( function(elem){ return elem.getAttribute("href"); }   ,  createChallengeElement)
+    createChallengeLink = "https://www.hackerrank.com"+createChallengeLink;
+    // console.log(createChallengeLink);
+
+    // add one challenge
+    // tab , challenge
+    let challenge = challenges[0];
+    // {
+    //     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+    //     "Description": "Question 1",
+    //     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+    //     "Input Format": "Integer",
+    //     "Constraints": "n <= 10 ^ 9",
+    //     "Output Format": "String",
+    //     "Tags": "Basics",
+    //   }
+
+
+};
+
+login();

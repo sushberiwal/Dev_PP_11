d132a041a889baadeef099b7a92d40e4b3085065
sushant <sushantberiwal@gmail.com>
2021-05-31 18:51:24 +0530

    moderator Add started

---
+++ Module1/Lec9_Async_Await/activity/moderatorAdd.js
@@ -0,0 +1,28 @@
+const id = "pamico3332@nic58.com";
+const pw = "12345678";
+const puppeteer = require("puppeteer");
+
+async function login(){
+    let browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ["--start-maximized"]
+      });
+    let pages = await browser.pages();
+    let tab = pages[0];
+    await tab.goto("https://www.hackerrank.com/auth/login");
+    await tab.type("#input-1", id);
+    await tab.type("#input-2", pw);
+    await tab.click( ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+    await tab.waitForSelector('div[data-analytics="NavBarProfileDropDown"]' , {visible:true});
+    await tab.waitForTimeout(2000);
+    let element = await tab.$('div[data-analytics="NavBarProfileDropDown"]');
+    await element.click();
+    await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
+    await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
+    await tab.waitForSelector('.nav-tabs.nav.admin-tabbed-nav li' , {visible:true});
+    let bothLis = await tab.$$('.nav-tabs.nav.admin-tabbed-nav li');
+    let manageChallengeLi = bothLis[1];
+    await manageChallengeLi.click();
+};
+login();

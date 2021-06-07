9e2684f8709d7539ac67d6d0ad9ef382952c62f7
sushant <sushantberiwal@gmail.com>
2021-06-07 20:54:34 +0530

    appendTodo complete

--- Module2/Todo_App/activity/script.js
+++ Module2/Todo_App/activity/script.js
@@ -1,6 +1,6 @@
 let addTodoButton = document.querySelector(".add-todo");
 let todoInput = document.querySelector(".todo-input");
-
+let todosList = document.querySelector(".todos-list-container");
 
 
 todoInput.addEventListener("keypress" , function(e){
@@ -17,9 +17,29 @@ addTodoButton.addEventListener( "click" , function(){
 function addTodo(){
     let todoInputValue = todoInput.value;
     if(todoInputValue){
-            console.log(todoInputValue);
+            appendTodo(todoInputValue);
             // it will empty the todoInput
             todoInput.value = "";
-        }
+    }
 }
+
+function appendTodo(todo){
+    let todoItemDiv = document.createElement("div");
+    todoItemDiv.classList.add("todo-item");
+    // <div class="todo-item"> </div>
+
+    let pTag = document.createElement("p");
+    pTag.classList.add("todo-input");
+    pTag.textContent = todo;
+    // <p class="todo-input">Learn Css</p>
+
+    let deleteTodoButton = document.createElement("button");
+    deleteTodoButton.classList.add("delete-todo");
+    deleteTodoButton.textContent = "Delete";
+    // <button class="delete-todo">Delete</button>
+
+    todoItemDiv.append(pTag);
+    todoItemDiv.append(deleteTodoButton);
     
+    todosList.append(todoItemDiv);
+}

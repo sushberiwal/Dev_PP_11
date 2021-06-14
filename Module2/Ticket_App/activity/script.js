b56068ab33eaa327770869bbd8e21c1de4f2603b
sushant <sushantberiwal@gmail.com>
2021-06-14 19:33:23 +0530

    ticket Append done

--- Module2/Ticket_App/activity/script.js
+++ Module2/Ticket_App/activity/script.js
@@ -38,19 +38,22 @@ function openTicketModal(e) {
     </div>`;
   document.querySelector("body").append(ticketModal);
   ticketModalOpen = true;
+  isTextTyped = false;
 
   let ticketTextDiv = ticketModal.querySelector(".ticket-text");
-  ticketTextDiv.addEventListener("keypress" , handleKeyPress );
+  ticketTextDiv.addEventListener("keypress", handleKeyPress);
 
   let ticketFilters = ticketModal.querySelectorAll(".ticket-filter");
-  for(let i=0 ; i<ticketFilters.length ; i++){
-      ticketFilters[i].addEventListener("click" , function(e){
-          if(e.target.classList.contains("selected-filter")){
-              return;
-          }
-          document.querySelector(".selected-filter").classList.remove("selected-filter");
-          e.target.classList.add("selected-filter");
-      })
+  for (let i = 0; i < ticketFilters.length; i++) {
+    ticketFilters[i].addEventListener("click", function (e) {
+      if (e.target.classList.contains("selected-filter")) {
+        return;
+      }
+      document
+        .querySelector(".selected-filter")
+        .classList.remove("selected-filter");
+      e.target.classList.add("selected-filter");
+    });
   }
 }
 
@@ -63,8 +66,37 @@ function closeTicketModal(e) {
 }
 
 function handleKeyPress(e) {
-  if(!isTextTyped){
-      isTextTyped = true;
-      e.target.textContent="";
-  } 
+  if (e.key == "Enter" && isTextTyped && e.target.textContent) {
+    let filterSelected =
+      document.querySelector(".selected-filter").classList[1];
+    let ticketInfoObject = {
+      ticketFilter: filterSelected,
+      ticketValue: e.target.textContent,
+    };
+    appendTicket(ticketInfoObject);
+    closeModal.click();
+  }
+
+  if(!isTextTyped) {
+    isTextTyped = true;
+    e.target.textContent = "";
+  }
+}
+
+function appendTicket(ticketInfoObject) {
+  let {ticketFilter , ticketValue} = ticketInfoObject;
+  let ticketDiv = document.createElement("div");
+  ticketDiv.classList.add("ticket");
+  ticketDiv.innerHTML = `<div class="ticket-header ${ticketFilter}"></div>
+  <div class="ticket-content">
+      <div class="ticket-info">
+          <div class="ticket-id">#e2nf5</div>
+          <div class="ticket-delete">
+              <i class="fas fa-trash"></i>
+          </div>
+      </div>
+      <div class="ticket-value">${ticketValue}</div>
+  </div>`;
+  
+  ticketsContainer.append(ticketDiv);
 }

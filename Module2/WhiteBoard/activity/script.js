d2d324d4dcc00756656f0421eee90f115131d18d
sushant <sushantberiwal@gmail.com>
2021-06-16 20:07:22 +0530

    tools css

--- Module2/WhiteBoard/activity/script.js
+++ Module2/WhiteBoard/activity/script.js
@@ -14,3 +14,29 @@ window.addEventListener("resize", function () {
 let ctx = canvas.getContext("2d");
 
 
+let isPenDown = false;
+
+canvas.addEventListener("mousedown" , function(e){
+    console.log("Inside mouse down");
+    isPenDown = true;
+    let x = e.clientX;
+    let y = e.clientY-100;
+    ctx.beginPath();
+    ctx.moveTo(x , y);
+})
+
+canvas.addEventListener("mousemove" , function(e){
+    if(isPenDown){
+        console.log("Inside mousemove")
+        let x = e.clientX;
+        let y = e.clientY-100;
+        ctx.lineTo(x , y);
+        ctx.stroke();
+    }
+})
+
+canvas.addEventListener("mouseup" , function(e){
+    console.log("mouseup");
+    isPenDown = false;
+})
+

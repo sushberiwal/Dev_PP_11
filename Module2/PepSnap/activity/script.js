557a29f9f0fa975da0b70130edf48fab91cbc494
sushant <sushantberiwal@gmail.com>
2021-06-22 19:41:12 +0530

    got video object

--- Module2/PepSnap/activity/script.js
+++ Module2/PepSnap/activity/script.js
@@ -1,10 +1,38 @@
 let videoElement = document.querySelector("video");
+let recordButton = document.querySelector("#record");
+let recordingState = false;
+let mediaRecorder;
 
-let constraint = { video: true };
-navigator.mediaDevices.getUserMedia(constraint).then(function(mediaStream) {
-    // console.log(mediaStream);
-    videoElement.srcObject = mediaStream;
-  })
-  .catch(function (error) {
-    console.log(error);
+(async function () {
+  let constraint = { video: true };
+  let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
+  videoElement.srcObject = mediaStream;
+  mediaRecorder = new MediaRecorder(mediaStream);
+  mediaRecorder.onstart = function () {
+    console.log("Inside on start");
+  };
+  mediaRecorder.ondataavailable = function (e) {
+    console.log("Inside on data available");
+    console.log(e.data);
+    let videoObject = new Blob([e.data], { type: "video/mp4" });
+    console.log(videoObject);
+  };
+  mediaRecorder.onstop = function () {
+    console.log("Inside on stop");
+  };
+
+  recordButton.addEventListener("click", function () {
+    if (recordingState) {
+      // already recording is going on
+      // stop the recording
+      mediaRecorder.stop();
+      recordButton.innerHTML = "Record Video";
+      recordingState=false;
+    } else {
+      // start the recording
+      mediaRecorder.start();
+      recordButton.innerHTML = "Recording..";
+      recordingState=true;
+    }
   });
+})();

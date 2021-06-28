799a3640913b787227b3ce61fb667d1a6dc1d5a8
sushant <sushantberiwal@gmail.com>
2021-06-28 20:25:25 +0530

    mediaFetch and show on UI

--- Module2/PepSnap/activity/gallery.js
+++ Module2/PepSnap/activity/gallery.js
@@ -1,19 +1,17 @@
 let db;
-
 let dbOpenRequest = indexedDB.open("Gallery", 1);
 dbOpenRequest.onupgradeneeded = function (e) {
   db = e.target.result;
   db.createObjectStore("Media", { keyPath: "mid" }); // table will only be create when db is create first time
 };
-
 dbOpenRequest.onsuccess = function (e) {
   db = e.target.result;
   fetchMedia();
 };
-
 dbOpenRequest.onerror = function (e) {
   alert("Inside on error !!");
 };
+
 
 function fetchMedia() {
   let txnObject = db.transaction("Media", "readonly");
@@ -33,12 +31,25 @@ function fetchMedia() {
   };
 }
 
-
-function appendPhoto(mediaObj){
-    let mediaDiv = document.createElement("div");
-    
+function appendPhoto(mediaObj) {
+  let mediaDiv = document.createElement("div");
+  mediaDiv.classList.add("media-div");
+  mediaDiv.innerHTML = `<img class="media-img" src=${mediaObj.url} alt="">
+    <div class="media-buttons">
+        <div class="download-media">Download</div>
+        <div class="delete-media">Delete</div>
+    </div>`;
+  document.querySelector(".gallery").append(mediaDiv);
 }
 
-function appendVideo(mediaObj){
-
+function appendVideo(mediaObj) {
+  let mediaDiv = document.createElement("div");
+  mediaDiv.classList.add("media-div");
+  mediaDiv.innerHTML = `<video class="media-video" controls></video>
+    <div class="media-buttons">
+        <div class="download-media">Download</div>
+        <div class="delete-media">Delete</div>
+    </div>`;
+    mediaDiv.querySelector("video").src = URL.createObjectURL(mediaObj.url);
+    document.querySelector(".gallery").append(mediaDiv);
 }

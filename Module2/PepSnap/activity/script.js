36f75fff59dd134def9c9915e5183a5f996b5a04
sushant <sushantberiwal@gmail.com>
2021-06-28 18:32:49 +0530

    photo and video saved in db

--- Module2/PepSnap/activity/script.js
+++ Module2/PepSnap/activity/script.js
@@ -29,64 +29,20 @@ let mediaRecorder;
     // videoObject/imageObject => URL
     // aTag
 
-    let videoURL = URL.createObjectURL(videoObject);
-    let aTag = document.createElement("a");
-    aTag.download = `Video${Date.now()}.mp4`;
-    aTag.href = videoURL;
-    aTag.click();
+    // let videoURL = URL.createObjectURL(videoObject);
+    // let aTag = document.createElement("a");
+    // aTag.download = `Video${Date.now()}.mp4`;
+    // aTag.href = videoURL;
+    // aTag.click();
+
+    // add video object to db
+    addMedia(videoObject , "video");
   };
   mediaRecorder.onstop = function () {
     console.log("Inside on stop");
   };
-
-  recordButton.addEventListener("click", function () {
-    if (recordingState) {
-      // already recording is going on
-      // stop the recording
-      mediaRecorder.stop();
-      recordingState = false;
-      recordButton.classList.remove("animate-record");
-    } else {
-      // start the recording
-      mediaRecorder.start();
-      recordingState = true;
-      recordButton.classList.add("animate-record");
-    }
-  });
-
-  capturePhoto.addEventListener("click", function () {
-    capturePhoto.classList.add("animate-capture");
-
-    setTimeout(function () {
-      capturePhoto.classList.remove("animate-capture");
-    }, 1000);
-
-    //   canvas
-    let canvas = document.createElement("canvas");
-    canvas.width = 640; //video width
-    canvas.height = 480; // video height
-
-    let ctx = canvas.getContext("2d");
-
-    if(currentZoom != 1){
-      ctx.translate(canvas.width/2 , canvas.height/2);
-      ctx.scale(currentZoom , currentZoom);
-      ctx.translate(-canvas.width/2 , -canvas.height/2);
-    }
-
-    ctx.drawImage(videoElement, 0, 0);
-
-    if (filterSelected != "none") {
-      ctx.fillStyle = filterSelected;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    // download canvas as an image
-    let aTag = document.createElement("a");
-    aTag.download = `Image${Date.now()}.jpg`;
-    aTag.href = canvas.toDataURL("image/jpg");
-    aTag.click();
-  });
+  recordButton.addEventListener("click", recordMediaFun);
+  capturePhoto.addEventListener("click", capturePhotoFun);
 })();
 
 for (let i = 0; i < filters.length; i++) {
@@ -134,3 +90,69 @@ zoomOut.addEventListener("click", function () {
   currentZoom = currentZoom - 0.1;
   videoElement.style.transform = `scale(${currentZoom})`;
 });
+
+function capturePhotoFun() {
+  capturePhoto.classList.add("animate-capture");
+
+  setTimeout(function () {
+    capturePhoto.classList.remove("animate-capture");
+  }, 1000);
+
+  //   canvas
+  let canvas = document.createElement("canvas");
+  canvas.width = 640; //video width
+  canvas.height = 480; // video height
+
+  let ctx = canvas.getContext("2d");
+
+  if (currentZoom != 1) {
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.scale(currentZoom, currentZoom);
+    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+  }
+
+  ctx.drawImage(videoElement, 0, 0);
+
+  if (filterSelected != "none") {
+    ctx.fillStyle = filterSelected;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  // download canvas as an image
+  // let aTag = document.createElement("a");
+  // aTag.download = `Image${Date.now()}.jpg`;
+  // aTag.href = canvas.toDataURL("image/jpg");
+  // aTag.click();
+
+  // save image to DB
+  let canvasURL = canvas.toDataURL("image.jpg");
+  addMedia(canvasURL, "photo");
+}
+
+function recordMediaFun() {
+  if (recordingState) {
+    // already recording is going on
+    // stop the recording
+    mediaRecorder.stop();
+    recordingState = false;
+    recordButton.classList.remove("animate-record");
+  } else {
+    // start the recording
+    mediaRecorder.start();
+    recordingState = true;
+    recordButton.classList.add("animate-record");
+  }
+}
+
+function addMedia(mediaURL, mediaType) {
+  //   db me media add hojaega
+  let txnObject = db.transaction("Media", "readwrite"); // start transaction on mediaTable
+  let mediaTable = txnObject.objectStore("Media"); // this will get access to mediaTable
+
+  mediaTable.add({ mid: Date.now(), type: mediaType, url: mediaURL }); // it will add this object in mediaTable or mediaStore
+
+  txnObject.onerror = function (e) {
+    console.log("txn failed");
+    console.log(e);
+  };
+}

2526c27dd4efb270aae7ae134a3eb2fd33058293
sushant <sushantberiwal@gmail.com>
2021-06-28 20:52:29 +0530

    download and delete implemented

--- Module2/PepSnap/activity/gallery.js
+++ Module2/PepSnap/activity/gallery.js
@@ -12,7 +12,6 @@ dbOpenRequest.onerror = function (e) {
   alert("Inside on error !!");
 };
 
-
 function fetchMedia() {
   let txnObject = db.transaction("Media", "readonly");
   let mediaTable = txnObject.objectStore("Media");
@@ -39,17 +38,59 @@ function appendPhoto(mediaObj) {
         <div class="download-media">Download</div>
         <div class="delete-media">Delete</div>
     </div>`;
+  mediaDiv
+    .querySelector(".download-media")
+    .addEventListener("click", function () {
+      downloadMedia(mediaObj);
+    });
+  mediaDiv
+    .querySelector(".delete-media")
+    .addEventListener("click", function () {
+      deleteMedia(mediaObj, mediaDiv);
+    });
+
   document.querySelector(".gallery").append(mediaDiv);
 }
 
 function appendVideo(mediaObj) {
   let mediaDiv = document.createElement("div");
   mediaDiv.classList.add("media-div");
-  mediaDiv.innerHTML = `<video class="media-video" controls></video>
+  mediaDiv.innerHTML = `<video class="media-video" controls autoplay loop></video>
     <div class="media-buttons">
         <div class="download-media">Download</div>
         <div class="delete-media">Delete</div>
     </div>`;
-    mediaDiv.querySelector("video").src = URL.createObjectURL(mediaObj.url);
-    document.querySelector(".gallery").append(mediaDiv);
+  mediaDiv.querySelector("video").src = URL.createObjectURL(mediaObj.url);
+  mediaDiv
+    .querySelector(".download-media")
+    .addEventListener("click", function () {
+      downloadMedia(mediaObj);
+    });
+  mediaDiv
+    .querySelector(".delete-media")
+    .addEventListener("click", function () {
+      deleteMedia(mediaObj, mediaDiv);
+    });
+  document.querySelector(".gallery").append(mediaDiv);
+}
+
+function downloadMedia(mediaObject) {
+  let aTag = document.createElement("a");
+  if (mediaObject.type == "photo") {
+    aTag.download = `${mediaObject.mid}.jpg`;
+    aTag.href = mediaObject.url;
+  } else {
+    aTag.download = `${mediaObject.mid}.mp4`;
+    aTag.href = URL.createObjectURL(mediaObject.url);
+  }
+  aTag.click();
+}
+
+function deleteMedia(mediaObject , mediaDiv) {
+  let mid = mediaObject.mid;
+  let txnObject = db.transaction("Media", "readwrite");
+  let mediaTable = txnObject.objectStore("Media");
+  mediaTable.delete(mid);
+
+  mediaDiv.remove();
 }

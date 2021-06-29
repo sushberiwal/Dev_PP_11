43efda01242544c3047e0b2537e60cb8bd677dff
sushant <sushantberiwal@gmail.com>
2021-06-29 20:31:56 +0530

    solveFormula done

--- Module2/Excel/script.js
+++ Module2/Excel/script.js
@@ -1,6 +1,10 @@
 let topLeftCell = document.querySelector(".top-left-cell");
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
+let address = document.querySelector("#address");
+let formulaInput = document.querySelector("#formula");
+let allCells = document.querySelectorAll(".cell");
+let lastSelectedCell;
 
 cellsContainer.addEventListener("scroll", function (e) {
   let topOffset = e.target.scrollTop;
@@ -12,16 +16,71 @@ cellsContainer.addEventListener("scroll", function (e) {
   leftCol.style.left = leftOffset + "px";
 });
 
-let allCells = document.querySelectorAll(".cell");
+formulaInput.addEventListener("blur", function (e) {
+  let formula = e.target.value;
+  if (formula) {
+    let calculatedValue = solveFormula(formula);
+    // UI Update
+    lastSelectedCell.textContent = calculatedValue;
+    // DB Update
+    let cellObject = getCellObjectFromElement(lastSelectedCell);
+    cellObject.value = calculatedValue;
+    cellObject.formula = formula;
+  }
+});
 
 for (let i = 0; i < allCells.length; i++) {
-  allCells[i].addEventListener("blur", function (e) {
-    // logic to save this value in db
-    let rowId = e.target.getAttribute("rowid");
-    let colId = e.target.getAttribute("colid");
-    let cellValueFromUI = e.target.textContent;
-    // cellObject ki value update !!
-    let cellObject = db[rowId][colId];
-    cellObject.value = cellValueFromUI;
+  allCells[i].addEventListener("click", function (e) {
+    let cellObject = getCellObjectFromElement(e.target);
+    address.value = cellObject.name;
+    formulaInput.value = cellObject.formula;
   });
+
+  allCells[i].addEventListener("blur", function (e) {
+    lastSelectedCell = e.target;
+    // logic to save this value in db
+    let cellValueFromUI = e.target.textContent;
+    if (cellValueFromUI) {
+      // cellObject ki value update !!
+      let cellObject = getCellObjectFromElement(e.target);
+      cellObject.value = cellValueFromUI;
+    }
+  });
+}
+
+function solveFormula(formula) {
+  // tip : implement infix evalutaion
+  // ( A1 + A2 ) => ( 10 + 20 );
+  let formulaComps = formula.split(" ");
+  // ["(" , "A1" , "+" , "A2" , ")"];
+  // find valid component
+  for (let i = 0; i < formulaComps.length; i++) {
+    let fComp = formulaComps[i];
+    if (
+      (fComp[0] >= "A" && fComp[0] <= "Z") ||
+      (fComp[0] >= "a" && fComp <= "z")
+    ) {
+      // A1 || A2
+      // fComp = A1
+      let cellObject = getCellObjectFromName(fComp);
+      let value = cellObject.value;
+      formula = formula.replace(fComp, value);
+    }
+  }
+  // ( 10 + 20 ) => infix evaluation
+  let calculatedValue = eval(formula);
+  return calculatedValue;
+}
+
+function getCellObjectFromElement(element) {
+  let rowId = element.getAttribute("rowid");
+  let colId = element.getAttribute("colid");
+  return db[rowId][colId];
+}
+
+function getCellObjectFromName(name) {
+  // A100
+  let colId = name.charCodeAt(0) - 65;
+  let rowId = Number(name.substring(1)) - 1;
+  return db[rowId][colId];
 }

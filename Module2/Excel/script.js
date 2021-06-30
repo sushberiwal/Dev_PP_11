6b8c7485a5880929a244c5bb04639b53e2407537
sushant <sushantberiwal@gmail.com>
2021-06-30 19:54:08 +0530

    formula to formula edge case

--- Module2/Excel/script.js
+++ Module2/Excel/script.js
@@ -20,6 +20,10 @@ formulaInput.addEventListener("blur", function (e) {
   let formula = e.target.value;
   if (formula) {
     let cellObject = getCellObjectFromElement(lastSelectedCell);
+    if(cellObject.formula != formula){
+      deleteFormula(cellObject);
+    }
+    
     let calculatedValue = solveFormula(formula, cellObject);
     // UI Update
     lastSelectedCell.textContent = calculatedValue;
